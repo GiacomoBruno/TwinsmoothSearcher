1f38703c5a253bc97ab1fd69661450e9fe5d0332
GiacomoBruno <giako13@gmail.com>
2022-10-20 20:22:04 +0200

    added compression for output

--- src/splitfile.cpp
+++ src/splitfile.cpp
@@ -1,5 +1,6 @@
 #include "splitfile.hpp"
 #include <filesystem>
+#include <zip_file.hpp>
 splitfile &splitfile::operator<<(const std::string &msg)
 {
     _current_file << msg;
@@ -8,8 +9,13 @@ splitfile &splitfile::operator<<(const std::string &msg)
     if(_current_file_size > _size)
     {
         _current_file.close();
+        miniz_cpp::zip_file zip;
+        zip.write(get_current_file_path() + ".txt");
+        zip.save(get_current_file_path() + ".zip");
+        std::filesystem::remove(get_current_file_path() + ".txt");
+
         _current_file_index++;
-        _current_file = std::ofstream(get_current_file_path());
+        _current_file = std::ofstream(get_current_file_path() + ".txt");
         _current_file_size = 0;
     }
 
@@ -19,23 +25,31 @@ splitfile &splitfile::operator<<(const std::string &msg)
 void splitfile::init()
 {
     std::string path{};
+    std::string zip_path{};
     do
     {
-        path = get_current_file_path();
+        path = get_current_file_path() + ".txt";
+        zip_path = get_current_file_path() + ".zip";
+
         _current_file_index++;
-    }while(std::filesystem::exists(path));
+    }while(std::filesystem::exists(zip_path) || std::filesystem::exists(path));
 
     _current_file_index--;
     if(_current_file_index == 0) {
-        _current_file = std::ofstream(get_current_file_path());
+        _current_file = std::ofstream(get_current_file_path() + ".txt");
         return;
     }
 
     _current_file_index--;
 
-    _current_file_size = std::filesystem::file_size(get_current_file_path());
+    _current_file_size = std::filesystem::file_size(get_current_file_path() + ".txt");
 
     if(_current_file_size > _size) {
+
+        miniz_cpp::zip_file zip;
+        zip.write(get_current_file_path() + ".txt");
+        zip.save(get_current_file_path() + ".zip");
+        std::filesystem::remove(get_current_file_path() + ".txt");
         _current_file_index++;
         _current_file_size = 0;
         _current_file = std::ofstream(get_current_file_path());
@@ -47,5 +61,5 @@ void splitfile::init()
 }
 std::string splitfile::get_current_file_path()
 {
-    return _folder + _filename + "_" + std::to_string(_current_file_index) + ".txt";
+    return _folder + _filename + "_" + std::to_string(_current_file_index);
 }
