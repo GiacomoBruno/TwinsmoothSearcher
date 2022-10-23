e51a99e9133b4a57024537fcca3207e518514eba
GiacomoBruno <giako13@gmail.com>
2022-10-23 11:19:23 +0200

    removed useless comment

--- src/main.cpp
+++ src/main.cpp
@@ -104,11 +104,6 @@ int main(int argc, char** argv)
     default:
         break;
     }
-    /*
-     *
-FOUND: 339282
-Total time taken: 19.8176 seconds
-     */
     b.conclude_bench();
     std::cout << "Total time taken: " << b.seconds_passed() << " seconds" << std::endl;
     std::string output_fd = "./result/";
